f3a82d77ce326f95b69fbf1126ab74dc46195968
Arthur Martins <arthurcarbonari99@gmail.com>
2023-01-01 04:17:37 +0000

    Implement unpacking of args before passing to fzf

--- zf.ps1
+++ zf.ps1
@@ -18,7 +18,7 @@ $foldersSet = [System.Collections.Generic.HashSet[string]]@($rootFolders)
 
 $rootFolders | ForEach-Object {Get-ChildItem $_ -Directory} | Select-Object FullName | ForEach-Object {$foldersSet.Add($_.FullName) | Out-Null}
 
-$selectedFolder = ($foldersSet | fzf $args)
+$selectedFolder = ($foldersSet | fzf @args)
 
 if(!$selectedFolder){
     exit

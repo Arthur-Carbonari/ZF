03ee0a90c894ef53127562b2a44c698fc086dbff
Arthur Martins <arthurcarbonari99@gmail.com>
2023-01-01 03:19:10 +0000

    Implement 'add' subcommand

--- zf.ps1
+++ zf.ps1
@@ -2,6 +2,18 @@ $myFoldersPath = Join-Path $PSScriptRoot .\myFolders.txt
 
 $rootFolders = Get-Content $myFoldersPath
 
+if ($args -contains "add") {
+    $cwd = (Get-Location).Path
+
+    if ($cwd -in $rootFolders){
+        return
+    }
+
+    $rootFolders += $cwd
+    $rootFolders | Out-File $myFoldersPath
+    return
+}
+
 $foldersSet = [System.Collections.Generic.HashSet[string]]@($rootFolders)
 
 $rootFolders | ForEach-Object {Get-ChildItem $_ -Directory} | Select-Object FullName | ForEach-Object {$foldersSet.Add($_.FullName) | Out-Null}
